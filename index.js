bcde763b2f4636e10ebf0163b9965ce4ea292265
jaygupta18 <jaykumarguptajti@gmail.com>
2026-01-03 21:49:29 +0530

    fixing explore error

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const PORT = process.env.PORT || 5000
 // Connect to MongoDB
 await connectDB()
 
-// Middleware
 app.use(cors())
 app.use(express.json())
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
